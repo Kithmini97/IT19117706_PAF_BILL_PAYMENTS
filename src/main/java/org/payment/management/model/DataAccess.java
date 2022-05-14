edc79be759af5a229d440229e0d069c371ff4ced
Kithmini97 <80144729+Kithmini97@users.noreply.github.com>
2022-05-14 23:42:54 +0530

    Add files

---
+++ src/main/java/org/payment/management/model/DataAccess.java
@@ -0,0 +1,31 @@
+package org.payment.management.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DataAccess {
+
+    private static Connection con;
+
+    public static Connection connect() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");//get java file
+
+            if (con == null) {
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Payment_management?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "Optimize@4321");
+                System.out.println(con);
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("sql");
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
